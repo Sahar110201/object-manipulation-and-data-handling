045d89a60132b16382bf1dc5a572442b514030da
Sahar110201 <saharadeeb7@gmail.com>
2025-11-21 16:47:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ const student = {
 console.log(student.name);
 console.log(student.age);
 console.log(student.infoDisplay());
+
+// part 2 converting to JSON String 
+const jsonString = JSON.stringify(student);
+console.log("JSON String:", jsonString);
+
+const studentNew = JSON.parse(jsonString);
+console.log("Object:", studentNew);
+
+console.log("original object:", student);
+console.log("compare:", student === studentNew);
+
